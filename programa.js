11f77be03be1e2d02f229845b85b6eeb049bd445
RubenMillan95 <necroptich@gmail.com>
2023-05-17 23:54:58 -0500

    version 0.0.1

---
+++ programa.js
@@ -0,0 +1,36 @@
+class teclado{
+    generarTeclado(){
+      const miTeclado=document.getElementById("teclado");
+
+      for(let i=65; i<=90; i++){
+        const letra= String.fromCharCode(i);
+
+        this.crearBoton(letra, miTeclado);
+
+        if(i==78){this.crearBoton("Ñ", miTeclado);}
+      }
+    }
+
+    crearBoton(letra, contenedor){
+
+        //TO DO: crear boton
+        console.log(letra);
+        const btn= document.createElement("button");
+        btn.id=letra;
+        btn.textContent=letra;
+         
+        btn.classList.add("btn", "btn-primary", "m-1");
+
+        btn.addEventListener("click", ()={this.presionarBoton(letra);
+        }); 
+        contenedor.appendChild(btn);
+    }
+
+    presionarBoton(){
+
+    }
+}
+
+const teclado=new Teclado;
+
+teclado.generarTeclado();
